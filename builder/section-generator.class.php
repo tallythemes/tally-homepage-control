25e01520f67c912413bf22d0beb653043786b987
Sazzad Hu <dev@sazzadh.com>
2016-03-24 23:27:17 +0600

    Improve CSS

--- builder/section-generator.class.php
+++ builder/section-generator.class.php
@@ -28,6 +28,7 @@ class tallybuilder_section_metabox_generator{
 		$this->show_settings = $settings['show_settings'];
 		
 		add_action( 'add_meta_boxes', array($this, 'register_metabox') );
+		add_action( 'save_post', array($this, 'metabox_save') );
 	}
 	
 	
@@ -67,7 +68,20 @@ class tallybuilder_section_metabox_generator{
 	}
 	
 	function metabox_save($post_id){
+		$meta_id = $this->meta_id;
+ 
+		// Checks save status
+		$is_autosave = wp_is_post_autosave( $post_id );
+		$is_revision = wp_is_post_revision( $post_id );
+		$is_valid_nonce = ( isset( $_POST[ $this->div_id.'_nonce' ] ) && wp_verify_nonce( $_POST[ $this->div_id.'_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+	 
+		// Exits script depending on save status
+		if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+			return;
+		}
 		
+		
+		tallybuilder_metabox_form_save($post_id, $meta_id, 'wp_kses');
 	}
 	
 	function section_settings_html($meta_data, $post_id){
@@ -86,16 +100,19 @@ class tallybuilder_section_metabox_generator{
 	
 	
 	function row_settings_html($meta_data, $post_id, $row, $row_i){
+		
+		$row_layout = (isset($meta_data['row'.$row_i.'_layout'])) ? $meta_data['row'.$row_i.'_layout'] : '';
+		
 		if($row['show_settings'] == true){
 			echo 'Layout';
 			echo '<select name="'.$this->meta_id.'[row'.$row_i.'_layout]">';
-				echo '<option value="none">Disable</option>';
-				echo '<option value="12">Full</option>';
-				echo '<option value="6,6">1/2 and 1/2</option>';
-				echo '<option value="4,4,4">1/3 + 1/3 + 1/3</option>';
-				echo '<option value="3,3,3,3">1/4 + 1/4 + 1/4 + 1/4</option>';
-				echo '<option value="6,3,3">1/6 + 1/4 + 1/4 </option>';
-				echo '<option value="3,3,6">1/4 + 1/4 + 1/6</option>';
+				echo '<option '.selected( $row_layout, '0,0,0,0', false ).' value="0,0,0,0">Disable</option>';
+				echo '<option '.selected( $row_layout, '12,0,0,0', false ).' value="12,0,0,0">Full</option>';
+				echo '<option '.selected( $row_layout, '6,6,0,0', false ).' value="6,6,0,0">1/2 and 1/2</option>';
+				echo '<option '.selected( $row_layout, '4,4,4,0', false ).' value="4,4,4,0">1/3 + 1/3 + 1/3</option>';
+				echo '<option '.selected( $row_layout, '3,3,3,3', false ).' value="3,3,3,3">1/4 + 1/4 + 1/4 + 1/4</option>';
+				echo '<option '.selected( $row_layout, '6,3,3,0', false ).' value="6,3,3,0">1/6 + 1/4 + 1/4 </option>';
+				echo '<option '.selected( $row_layout, '3,3,6,0', false ).' value="3,3,6,0">1/4 + 1/4 + 1/6</option>';
 			echo '</select>';
 			echo '<a href="" class="tbmb_edit_row_setting" rel=".tbmb_row'.$row_i.'_settings">Customize Row</a>';
 			echo '<div style="display:none">';
@@ -119,8 +136,6 @@ class tallybuilder_section_metabox_generator{
 	
 	
 	function column_html($meta_data, $post_id, $row, $row_i, $column, $column_i){
-		
-		$meta_data['row'.$row_i.'_layout'] = '6,6,6';
 		
 		$grid_class = 'tbmb_col tbmb_col_';
 		$column_position = $column_i - 1;
